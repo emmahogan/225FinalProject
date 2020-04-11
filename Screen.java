e2456c66b2036ffcb72269a9223e01e03230a951
emmahogan <em30hoga@siena.edu>
2020-04-11 14:08:53 -0400

    Screen class

---
+++ Screen.java
@@ -0,0 +1,32 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+import javax.swing.event.*;
+
+/**
+ * Write a description of class TitleScreen here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Screen extends JPanel
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class TitleScreen
+     */
+    public Screen()
+    {
+        super();
+        
+    }
+    
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        
+    }
+}
