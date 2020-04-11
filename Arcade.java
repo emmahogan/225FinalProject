76df31ce86517e58c3850b07250e5731c4958d86
emmahogan <em30hoga@siena.edu>
2020-04-11 14:09:56 -0400

    Arcade

---
+++ Arcade.java
@@ -0,0 +1,29 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+import javax.swing.event.*;
+
+/**
+ * Write a description of class MainScreen here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Arcade extends MouseAdapter implements Runnable
+{
+    private JFrame frame = new JFrame();
+    private final int FRAME_WIDTH = 400;
+    private final int FRAME_HEIGHT = 400;
+    
+    public void run(){
+        frame.setDefaultLookAndFeelDecorated(true);
+        frame.setPreferredSize(new Dimension(FRAME_WIDTH, FRAME_HEIGHT));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        
+    }
+    public static void main(String args[]){
+        javax.swing.SwingUtilities.invokeLater(new Arcade());
+    }
+}
