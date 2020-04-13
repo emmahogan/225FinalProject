9b1dd9d6843a47b83a0e093493e74d64a9f35825
emmahogan <em30hoga@siena.edu>
2020-04-13 14:07:42 -0400

    added ChessBoard class

---
+++ chess/ChessBoard.java
@@ -0,0 +1,32 @@
+package chess;
+
+import gameutils.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+import javax.swing.event.*;
+
+/**
+ * Write a description of class ChessBoard here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class ChessBoard extends Screen
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class ChessBoard
+     */
+    public ChessBoard()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    public void render(Graphics g){}
+    public void update(){}
+}
