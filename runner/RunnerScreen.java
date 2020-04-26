1cfb219044c05588ff20ff40be1b42f0e5dbd862
AndrewTowse <a15tows@siena.edu>
2020-04-26 16:25:52 -0400

    Added contact method that checks for a collision and initialized all the objects

--- runner/RunnerScreen.java
+++ runner/RunnerScreen.java
@@ -2,13 +2,26 @@ package runner;
 
 import java.awt.Graphics;
 import gameutils.Screen;
+import java.util.ArrayList;
 
 public class RunnerScreen extends Screen{
 private Ball ball;
+private Walls walls;
+private Floor floor;
+private ArrayList<Gate> gates;
+private BallController controller;
+
+private boolean touched;
 
 public RunnerScreen (){
     super();
     this.ball = new Ball();
+    this.walls = new Walls();
+    this.floor = new Floor();
+    this.gates = new ArrayList<Gate>();
+    this.controller = new BallController(ball);
+    this.touched = false;
+
 
 }
 
@@ -23,6 +36,20 @@ public RunnerScreen (){
     }
 
     public void dispose(){
+
+
+    }
+
+    /**
+     * Method checks if ball contact was made with wall or gate
+     *
+     */
+
+    public void contact(){
+    if(walls.contains(ball) || gates.get(0).contains(ball)){
+        touched = true;
+
+    }
 
 
     }
