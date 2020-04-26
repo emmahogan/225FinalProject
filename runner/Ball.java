3880af0fdd14e24d705988b51d555e410ee3de47
AndrewTowse <a15tows@siena.edu>
2020-04-26 16:26:22 -0400

    Added new instance variable to Ball to make contains calculations easier

--- runner/Ball.java
+++ runner/Ball.java
@@ -4,12 +4,14 @@ import gameutils.GameObject;
 import java.awt.Point;
 
 public class Ball extends GameObject{
-
+private Point center;
+private int radius;
 
     public Ball(){
         super();
         position = new Point(100, 100);
-
+        radius = 50;
+        center = new Point(position.x + radius, position.y - radius);
     }
 
     public void update(){
