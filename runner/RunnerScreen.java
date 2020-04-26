91452dac86d3a902ce94be39b2dd32aa7bde629e
AndrewTowse <a15tows@siena.edu>
2020-04-26 14:38:17 -0400

    Added RunnerScreen and RunnerGame

---
+++ runner/RunnerScreen.java
@@ -0,0 +1,45 @@
+package runner;
+
+import java.awt.Graphics;
+import gameutils.Screen;
+
+public class RunnerScreen extends Screen{
+
+    /**
+     * Renders what is on the game screen.
+     * To be called in the thread of the game class
+     * @param g the Graphics from paintComponent
+     */
+    public void render(Graphics g){
+
+
+    }
+
+    /**
+     * Code that changes what is on the screen goes here.
+     * To be called in the thread of the game class
+     */
+    public void update(){
+
+
+    }
+
+    /**
+     * All objects that need to be disposed go here.
+     *
+     * Example:
+     * Let obj be a pointer to a GameObject
+     * obj = null;
+     *
+     * Typically, objects no longer needed should
+     * be placed in an ArrayList and be disposed as follows:
+     * for (GameObjects obj: removedObjs) {
+     *     obj = null;
+     * }
+     */
+    public void dispose(){
+
+
+    }
+
+}
