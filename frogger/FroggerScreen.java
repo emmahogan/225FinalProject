72f7287b7785d40eec7c972cb98ffc075099b780
ns30shel <ns30shel@siena.edu>
2020-04-29 21:34:06 -0400

    Created first set of classes needed.

---
+++ frogger/FroggerScreen.java
@@ -0,0 +1,33 @@
+package frogger;
+
+import java.awt.Graphics;
+import java.util.ArrayList;
+
+import gameutils.Screen;
+/**
+ * Write a description of class FroggerScreen here.
+ *
+ * @author Nick Shelby
+ * @version Spring 2020
+ */
+public class FroggerScreen extends Screen
+{
+    public FroggerScreen() {
+        
+    }
+    
+     @Override
+    public void render(Graphics g) {
+        
+    }
+    
+    @Override
+    public void update() {
+    
+    }
+    
+    @Override
+    public void dispose() {
+
+    }
+}
