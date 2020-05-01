4d90076da8d0996bbe53fbe0ce7eca90dc03164b
ns30shel <ns30shel@siena.edu>
2020-05-01 12:42:30 -0400

    Added Tile class, needs functionality.

---
+++ frogger/Tile.java
@@ -0,0 +1,20 @@
+package frogger;
+
+import gameutils.GameObject;
+
+import java.awt.Point;
+
+/**
+ * Write a description of class Tile here.
+ *
+ * @author Nick Shelby
+ * @version Spring 2020
+ */
+public class Tile extends GameObject {
+
+    @Override
+    public void update() {
+        //Add code to satisfy update if necessary
+    }
+
+}
