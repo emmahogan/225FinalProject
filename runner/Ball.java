3f4adb25621421e9735487800ba85f724d633540
AndrewTowse <a15tows@siena.edu>
2020-05-01 19:15:24 -0400

    The controller was the update issue

--- runner/Ball.java
+++ runner/Ball.java
@@ -10,6 +10,9 @@ public class Ball extends GameObject{
 private Point center;
 private int frameWidth;
 private int frameHeight;
+//FOR POSITIONING
+private int acceleration;
+private int xSpeed;
 
     public Ball(int frameWidth, int frameHeight){
         super();
@@ -19,15 +22,21 @@ private int frameHeight;
         this.frameHeight = frameHeight;
         position = new Point((frameWidth - (texture.getWidth()))/2, frameHeight - (texture.getHeight()) - 50);
         setBounds();
+        acceleration = 0;
+        xSpeed = 0;
     }
 
     public int getRadius(){
         return 0;
     }
 
+    public int getAcceleration(){return acceleration;}
+
+    public void setAcceleration(int acceleration){this.acceleration = acceleration;}
 
     public void update(){
-
+         xSpeed = xSpeed + acceleration;
+         position.x += xSpeed;
     }
 
 
