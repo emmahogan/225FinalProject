1291f0b68b934eb4d954db957504672ca3fd6789
Justin Marotta <ja08maro@siena.edu>
2020-05-01 15:25:39 -0400

    added Track

--- racing/Car.java
+++ racing/Car.java
@@ -2,19 +2,15 @@ package racing;
 
 import java.awt.Point;
 import java.awt.Rectangle;
-import java.awt.geom.Line2D;
-import java.util.ArrayList;
-
-import javax.sound.sampled.Line;
 
 import gameutils.GameObject;
 import gameutils.Texture;
 
 public class Car extends GameObject {
     public Rectangle top;
-    private Rectangle bottom;
-    private Rectangle leftSide;
-    private Rectangle rightSide;
+    public Rectangle bottom;
+    public Rectangle leftSide;
+    public Rectangle rightSide;
 
     private Texture up;
     private Texture down;
@@ -45,71 +41,25 @@ public class Car extends GameObject {
         checkRotation();
     }
 
-    public Point getPosition() {
-        return new Point((int) position.x, (int) position.y);
-    }
-
-    public void setPosition(Point newPosition) {
-        this.position = new FloatPoint(newPosition.x, newPosition.y);
-    }
-
-    private void setBoundsAndLines() {
-        bounds = new Rectangle((int) position.x, (int) position.y, texture.getWidth(), texture.getHeight());
-        top = new Rectangle(bounds.x + 7, bounds.y, bounds.width - 7, 1);
-        bottom = new Rectangle(bounds.x + 7, bounds.y + bounds.height - 1, bounds.width - 7, 1);
-        leftSide = new Rectangle(bounds.x, bounds.y + 7, 1, bounds.height - 7);
-        rightSide = new Rectangle(bounds.x + bounds.width - 1, bounds.y + 7, 1, bounds.height - 7);
-    }
-
-    public void collision(ArrayList<Track> tracks, Car other) {
-        boolean done = false;
-        for (int i = 0; i < tracks.size() && !done; i++) {
-            if (top.intersects(tracks.get(i).bounds) ||
-                    top.intersects(other.bounds)) {
-                done = true;
-                velocity.y = 0;
-                velocity.y += 1;
-            }
-            if (bottom.intersects(tracks.get(i).bounds) ||
-                    bottom.intersects(other.bounds)) {
-                done = true;
-                velocity.y = 0;
-                velocity.y -= 2;
-            }
-            if (leftSide.intersects(tracks.get(i).bounds) ||
-                    leftSide.intersects(other.bounds)) {
-                done = true;
-                velocity.x = 0;
-                velocity.x += 2;
-            }
-            if (rightSide.intersects(tracks.get(i).bounds) ||
-                    rightSide.intersects(other.bounds)) {
-                done = true;
-                velocity.x = 0;
-                velocity.x -= 2;
-            }
-        }
-    }
-
     private void moveByVelocity() {
         position.x += velocity.x;
         position.y += velocity.y;
     }
 
     private void checkRotation() {
-        if (velocity.x > 2) {
+        if (velocity.x > 1.5) {
             nextTexture = right;
             setBoundsAndLines();
         }
-        if (velocity.x < -2) {
+        if (velocity.x < -1.5) {
             nextTexture = left;
             setBoundsAndLines();
         }
-        if (velocity.y < -2) {
+        if (velocity.y < -1.5) {
             nextTexture = up;
             setBoundsAndLines();
         }
-        if (velocity.y > 2) {
+        if (velocity.y > 1.5) {
             nextTexture = down;
             setBoundsAndLines();
         }
@@ -134,25 +84,41 @@ public class Car extends GameObject {
         }
     }
 
+    public Point getPosition() {
+        return new Point((int) position.x, (int) position.y);
+    }
+
+    public void setPosition(Point newPosition) {
+        this.position = new FloatPoint(newPosition.x, newPosition.y);
+    }
+
+    private void setBoundsAndLines() {
+        bounds = new Rectangle((int) position.x, (int) position.y, texture.getWidth(), texture.getHeight());
+        top = new Rectangle(bounds.x, bounds.y, bounds.width, 1);
+        bottom = new Rectangle(bounds.x, bounds.y + bounds.height - 1, bounds.width, 1);
+        leftSide = new Rectangle(bounds.x, bounds.y, 1, bounds.height);
+        rightSide = new Rectangle(bounds.x + bounds.width - 1, bounds.y, 1, bounds.height);
+    }
+
     private void initTextures(String color) {
         if (color.equals("blue")) {
             up = new Texture("assets/racing/player1_up.png");
-            up.scale(.05, .05);
+            up.scale(.03, .03);
             down = new Texture("assets/racing/player1_down.png");
-            down.scale(.05, .05);
+            down.scale(.03, .03);
             left = new Texture("assets/racing/player1_left.png");
-            left.scale(.05, .05);
+            left.scale(.03, .03);
             right = new Texture("assets/racing/player1_right.png");
-            right.scale(.05, .05);
+            right.scale(.03, .03);
         } else {
             up = new Texture("assets/racing/player2_up.png");
-            up.scale(.05, .05);
+            up.scale(.03, .03);
             down = new Texture("assets/racing/player2_down.png");
-            down.scale(.05, .05);
+            down.scale(.03, .03);
             left = new Texture("assets/racing/player2_left.png");
-            left.scale(.05, .05);
+            left.scale(.03, .03);
             right = new Texture("assets/racing/player2_right.png");
-            right.scale(.05, .05);
+            right.scale(.03, .03);
         }
         texture = right;
         curTexture = right;
---
+++ racing/Wall.java
@@ -0,0 +1,53 @@
+package racing;
+
+import java.awt.Point;
+import java.awt.Rectangle;
+
+import gameutils.GameObject;
+
+public class Wall extends GameObject {
+    public static final int SIZE = 50;
+
+    public Wall(int x, int y) {
+        x = (x * SIZE);
+        y = SpeedRacers.FRAME_HEIGHT - (y * SIZE) - SIZE;
+        position = new Point(x, y);
+        setBounds(new Rectangle(position.x, position.y, 50, 50));
+    }
+
+    @Override
+    public void update() {
+        if (bounds.getLocation() != position) {
+            bounds.setLocation(position);
+        }
+    }
+
+    public void collision(Car p1, Car p2) {
+        Car currentCar = null;
+        if (this.collidesWith(p1)) {
+            currentCar = p1;
+        } else if (this.collidesWith(p2)) {
+            currentCar = p2;
+        }
+
+        if (currentCar != null) {
+            if (bounds.intersects(currentCar.rightSide)) {
+                currentCar.velocity.x = 0;
+                currentCar.velocity.x -= 2;
+            }
+            if (bounds.intersects(currentCar.bottom)) {
+                currentCar.velocity.y = 0;
+                currentCar.velocity.y -= 2;
+            }
+            if (bounds.intersects(currentCar.top)) {
+                currentCar.velocity.y = 0;
+                currentCar.velocity.y += 2;
+            }
+            if (bounds.intersects(currentCar.leftSide)) {
+                currentCar.velocity.x = 0;
+                currentCar.velocity.x += 2;
+            }
+        }
+    }
+}
+
