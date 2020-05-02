1fd8f945c10c3d208e89a963912ebcdb8230b13c
ns30shel <ns30shel@siena.edu>
2020-05-01 20:04:04 -0400

    Extended GameObject, still no functionality.

--- frogger/River.java
+++ frogger/River.java
@@ -1,10 +1,17 @@
 package frogger;
 
+import gameutils.GameObject;
+
 import java.util.ArrayList;
 
-public class River {
+public class River extends GameObject {
     //Write code to have each horizontal line of the river have a spawn rate for the logs.
     //The logs all move at the same speed, for now.
 
     private ArrayList<Log> logs;
+
+    @Override
+    public void update() {
+        //Add code to satisfy update if necessary
+    }
 }
--- frogger/Tile.java
+++ frogger/Tile.java
@@ -1,16 +1,23 @@
 package frogger;
 
 import gameutils.GameObject;
+import gameutils.Texture;
+import racing.FloatPoint;
 
 import java.awt.Point;
 
 /**
- * This will output the grass sprites.
+ * This will create the horizontal row of grass sprites.
  *
  * @author Nick Shelby
  * @version Spring 2020
  */
 public class Tile extends GameObject {
+
+    public Tile() {
+        super();
+        //Add code to construct grass stuff
+    }
 
     @Override
     public void update() {
