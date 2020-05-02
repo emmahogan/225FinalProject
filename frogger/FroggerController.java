da9788aa418333965e9de7e611454dd575d44f67
ns30shel <ns30shel@siena.edu>
2020-05-01 22:01:35 -0400

    Added methods to handle key input, needs testing.

--- frogger/FroggerController.java
+++ frogger/FroggerController.java
@@ -2,6 +2,9 @@ package frogger;
 
 import gameutils.Controller;
 import gameutils.Texture;
+
+import java.awt.event.KeyEvent;
+
 /**
  * Write a description of class FroggerController here.
  *
@@ -10,7 +13,22 @@ import gameutils.Texture;
  */
 public class FroggerController extends Controller
 {
+    Frog froggah;
+
+    public FroggerController (Frog frogToControl) {
+        super();
+        froggah = frogToControl;
+    }
+
     public void handleKeyInput() {
-        
+        if (isKeyPressed(W)) {
+            froggah.jumpForward();
+        } else if (isKeyPressed(S)) {
+            froggah.jumpBack();
+        } else if (isKeyPressed(A)) {
+            froggah.jumpLeft();
+        } else if (isKeyPressed(D)) {
+            froggah.jumpRight();
+        }
     }
 }
