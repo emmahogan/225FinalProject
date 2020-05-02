d4992e7263b8f7b2f08a38ace8e6e0db2e967b11
ns30shel <ns30shel@siena.edu>
2020-05-01 20:57:21 -0400

    Set the texture, added comments.

--- frogger/Tile.java
+++ frogger/Tile.java
@@ -2,12 +2,11 @@ package frogger;
 
 import gameutils.GameObject;
 import gameutils.Texture;
-import racing.FloatPoint;
 
 import java.awt.Point;
 
 /**
- * This will create the horizontal row of grass sprites.
+ * This will create the horizontal row of grass.
  *
  * @author Nick Shelby
  * @version Spring 2020
@@ -16,12 +15,14 @@ public class Tile extends GameObject {
 
     public Tile() {
         super();
+        texture = new Texture("assets/frogger/grass_line.png");
         //Add code to construct grass stuff
+        //Shouldn't really need much in here since it is just the grass, nothing special.
     }
 
     @Override
     public void update() {
-        //Add code to satisfy update if necessary
+        //May not need code in here.
     }
 
 }
