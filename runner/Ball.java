641e751c487014306d7bef3c6907a08736902459
emmahogan <em30hoga@siena.edu>
2020-05-04 15:36:17 -0400

    Added a lot of javadoc comments in chess and ball class of runner

--- runner/Ball.java
+++ runner/Ball.java
@@ -8,11 +8,18 @@ import java.awt.Rectangle;
 import java.util.ArrayList;
 import java.util.Random;
 
+/**
+ * This class represents an instance of the ball used in the runner game, which extends GameObject
+ *
+ * @author Justin, Andrew, Emma, Tim, Nick
+ * @version Spring 2020
+ */
 public class Ball extends GameObject {
-    public static int radius;
 
+    //frame measurements
     private int frameWidth;
     private int frameHeight;
+
     //FOR POSITIONING
     private double acceleration;
     private double xSpeed;
@@ -24,6 +31,9 @@ public class Ball extends GameObject {
     private Point bottomLeft;
     private Point upperRight;
     private Point bottomRight;
+
+    //array list holding all textures used for animation
+    private ArrayList<Texture> textures = new ArrayList<Texture>();
 
     //textures for animation
     private Texture ball1;
@@ -40,32 +50,60 @@ public class Ball extends GameObject {
     private Texture ball12;
     private Texture ball13;
 
+    //timer for animation
     private int animationClock;
+
+    //which texture
     private int pic;
 
-    private ArrayList<Texture> textures = new ArrayList<Texture>();
+    //the radius of the ball
+    public static int radius;
 
+
+    /**
+     * Constructor for Ball object sets its initial texture, and initializes all variables
+     * needed for animation and calculations for positioning
+     *
+     * @param frameWidth Width of frame
+     * @param frameHeight Height of frame
+     */
     public Ball(int frameWidth, int frameHeight) {
         super();
+
+        //Set to initial texture
         this.texture = new Texture("assets/runner/ball1.png");
         texture.scale(0.25, 0.25);
+
+        //Initialize variables used for animation
         animationClock = 0;
         pic = 0;
 
+        //Call method to scale all textures used for animation
+        scaleTextures();
+
+        //set input frame dimensions
         this.frameWidth = frameWidth;
         this.frameHeight = frameHeight;
 
+        //Calculate starting position horizontally centered towards bottom of screen
         position = new Point((frameWidth - (texture.getWidth())) / 2, frameHeight - (texture.getHeight()) - 50);
-        calculatePoints();
 
+        //Calculate corner points and set bounds
+        calculatePoints();
         setBounds();
+
+        //initialize variables used for speed and acceleration with the ball
+        //starting by accelerating to the left
         acceleration = -0.23;
         xSpeed = 0;
-        scaleTextures();
-        //initialize radius and center for collides with purposes
+
+        //initialize radius for collides with purposes
         radius = texture.getWidth()/2;
     }
 
+    /**
+     * Method to calculate the corners of the bounds based on the current position
+     */
     public void calculatePoints(){
         upperLeft = new Point(position.x, position.y);
         bottomLeft = new Point(position.x, position.y + texture.getHeight());
@@ -73,49 +111,79 @@ public class Ball extends GameObject {
         bottomRight = new Point(position.x + texture.getWidth(), position.y + texture.getHeight());
     }
 
+    /**
+     * The Ball's update method does the following:
+     *    - Calls method to change texture for animation effect
+     *    - Updates the speed and acceleration variables
+     *    - Then updates the position accordingly
+     *    - Calls method of superclass to update bounds
+     */
+    public void update() {
+        //for animation
+        changeTexture();
 
-    public int getRadius() {
-        return 0;
+        //updates variables for speed and acceleration
+        speedWait += 16;
+        xSpeed = xSpeed + acceleration;
+        speedWait = 0;
+
+        //updates positioning and bounds
+        position.x += xSpeed;
+        calculatePoints();
+        setBounds();
     }
 
+
+    ////////////////////////////// MODIFIERS AND ACCESSORS //////////////////////////////////////
+
+    /**
+     * Accessor method for current acceleration
+     *
+     * @return current acceleration
+     */
     public double getAcceleration() {
         return acceleration;
     }
 
-
-
+    /**
+     * Modifier method to set acceleration
+     *
+     * @param acceleration The acceleration to change to
+     */
     public void setAcceleration(double acceleration) {
         this.acceleration = acceleration;
     }
 
-
-
-    public void update() {
-        changeTexture();
-        speedWait += 16;
-        xSpeed = xSpeed + acceleration;
-        speedWait = 0;
-        position.x += xSpeed;
-        setBounds();
-        calculatePoints();
-
-    }
-
-    public void calcPixMove() {
-        currentMove = (int )xSpeed / (RunnerGame.waitTIme / 16);
-    }
-
+    /**
+     * This method neutralizes the ball's speed (sets to 0)
+     */
     public void neuterSpeed() {
         this.xSpeed = 0.0;
     }
 
 
+
+
+    ////////////////////////////////// COLLISION WITH GATES /////////////////////////////////////////
+
+
+    /**
+     * The first of 3 methods used to accurately detect the collision of the circular ball with
+     * the rectangular gates based on circumference of the ball, not the bounds
+     * of the rectangle encapsulating it
+     *
+     * @param rect The rectangle representing a gate
+     *
+     * @return true if the objects are colliding, false if not
+     */
     public boolean collidesWithRect(Rectangle rect){
-        Point rectUL = new Point(rect.x, rect.y);
-        Point rectUR = new Point((int)(rect.x + rect.getWidth()), rect.y);
+
+        //Bottom left corner of rectangle
         Point rectBL = new Point(rect.x, (int)(rect.y + rect.getHeight()));
+        //Bottom right corner of rectangle
         Point rectBR = new Point((int)(rect.x + rect.getWidth()), (int)(rect.y + rect.getHeight()));
 
+        //Call pair of helper methods to help with accurate detection of collision
         if(collisionHelper1(upperLeft, rect)){
             return collisionHelper2(upperLeft, rectBR);
         }
@@ -131,14 +199,30 @@ public class Ball extends GameObject {
         return false;
     }
 
+    /**
+     * First helper method for detecting collision between a ball and a gate
+     *
+     * @param corner Corner of ball's bounds
+     * @param rect The rectangle to check for collision with
+     *
+     * @return Whether they are colliding
+     */
     private boolean collisionHelper1(Point corner, Rectangle rect){
         if(corner.x >= rect.x && corner.x <= rect.x + rect.getWidth() && corner.y >= rect.y && corner.y <= rect.y + rect.getHeight() ){
             return true;
-
         }
         return false;
     }
 
+    /**
+     * Second helper method for detecting collision between a ball and a gate that
+     * now knows the specific corners of the ball and rectangle to check
+     *
+     * @param bCorner Corner of ball's bounds
+     * @param rCorner The rectangle's corner to check for collision with
+     *
+     * @return Whether they are colliding
+     */
     private boolean collisionHelper2(Point bCorner, Point rCorner){
         int dx = Math.abs(bCorner.x - rCorner.x);
         int dy = Math.abs(bCorner.y - rCorner.y);
@@ -149,7 +233,18 @@ public class Ball extends GameObject {
         return false;
     }
 
+
+    /////////////////////////////////////// ANIMATION ///////////////////////////////////////////////
+
+    /**
+     * This method for the animation is used to:
+     *    - Initialize all textures used for animation
+     *    - Add all of these textures to the arraylist
+     *    - Scale all of these textures to the correct size
+     */
     private void scaleTextures(){
+
+        //Initialize all textures
         ball1 = new Texture("assets/runner/ball1.png");
         ball2 = new Texture("assets/runner/ball2.png");
         ball3 = new Texture("assets/runner/ball3.png");
@@ -163,6 +258,8 @@ public class Ball extends GameObject {
         ball11 = new Texture("assets/runner/ball11.png");
         ball12 = new Texture("assets/runner/ball12.png");
         ball13 = new Texture("assets/runner/ball13.png");
+
+        //Add textures to array list
         textures.add(ball1);
         textures.add(ball2);
         textures.add(ball3);
@@ -176,36 +273,36 @@ public class Ball extends GameObject {
         textures.add(ball11);
         textures.add(ball12);
         textures.add(ball13);
+
+        //Scale all textures
         for(Texture t: textures) {
             t.scale(0.25, 0.25);
         }
     }
 
+    /**
+     * This method is called to switch the texture to create the animation effect
+     */
     public void changeTexture(){
-        /**
-        animationClock += 16;
-        if(animationClock >= 32) {
-            Random rand = new Random();
-            int x = rand.nextInt(13);
-            texture = textures.get(x);
-            setBounds();
-            calculatePoints();
-            animationClock = 0;
-        }
-         */
+        //Set variable accordingly that helps with timing of image switches
         animationClock += 16;
         if(animationClock >= 48) {
+            //keeping track of what picture number it is on, iterates through the array list of textures
             if(pic >= textures.size()){
                 pic = 0;
             }
+
+            //Switches texture
             texture = textures.get(pic);
+            pic++;
+
+            //resets bounds
             setBounds();
             calculatePoints();
-            pic++;
+
+            //Resets clock
             animationClock = 0;
         }
-
-
     }
 
 }
