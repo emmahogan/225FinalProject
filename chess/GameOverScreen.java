083012ff1c6a277f7ec8bb3b2cbc714961d3bc95
emmahogan <em30hoga@siena.edu>
2020-05-03 21:40:17 -0400

    can't execute changing to game over screen

--- chess/GameOverScreen.java
+++ chess/GameOverScreen.java
@@ -21,11 +21,20 @@ public class GameOverScreen extends Screen implements ActionListener {
     //Text color set to opposite of winning team's color
     private Color textColor;
 
-    //Time to wait between flashes of text in ms
-    private int flashWait;
+    //if text is flashing
+    boolean flashing = true;
+
+    //Time waiting between flashes of text
+    private int flashWait = 1000;
+    private int curTime = 0;
+
+    //Text font
+    Font msgFont = new Font("Helvetica", Font.BOLD, 50);
+    FontMetrics fontMetrics = getFontMetrics(msgFont);
 
     /**
-     * Constructs a new GameOverScreen with the appropriate winner
+     * Constructs a new GameOverScreen with the appropriate winner,
+     * and sets the Colors appropriately
      *
      * @param winner The team that won
      */
@@ -42,26 +51,61 @@ public class GameOverScreen extends Screen implements ActionListener {
         }
 
         this.winner += "Wins!!!";
+
+        //Play again button
+        this.setLayout(new BorderLayout());
+        JPanel bottomPanel = new JPanel();
+        this.add(bottomPanel, BorderLayout.SOUTH);
+        bottomPanel.add(newGame);
+
+
     }
 
     /**
+     * Method to repaint, calculating whether flashing then after done flashing just
+     * draws string
      *
      * @param g the Graphics from paintComponent
      */
     @Override
     public void render(Graphics g) {
-        //
+        //Draw text, calculating whether to draw currently if still flashing
+        if(flashing){
+            //calculates whether to flash on or off
+            curTime += 16;
+            if(curTime > flashWait){
+                flashWait += 1000;
+                //only flashes a few times, then stays solid
+                if(flashWait > 6000){
+                    flashing = false;
+                }
+            }
+            if(flashWait % 1000 != 1){
+                drawText(g);
+            }
+        //if not flashing, draw text
+        } else {
+            drawText(g);
+        }
+
+    }
+
+    /**
+     * Draws string with game over text saying the winning team
+     *
+     * @param g Graphics object
+     */
+    public void drawText(Graphics g){
+        g.setColor(textColor);
+        g.setFont(msgFont);
+        g.drawString(winner, (ChessBoard.FRAME_WIDTH - fontMetrics.stringWidth(winner)) / 2, ChessBoard.FRAME_WIDTH /3);
     }
 
     @Override
-    public void update() {
-
-    }
+    public void update() {}
 
     @Override
-    public void dispose() {
-
-    }
+    public void dispose() {}
 
     @Override
     public void actionPerformed(ActionEvent e) {
