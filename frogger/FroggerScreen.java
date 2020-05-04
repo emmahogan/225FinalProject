40ac7532a2e0b2c5e1af10fefc50bc760d280aa4
Justin Marotta <ja08maro@siena.edu>
2020-05-04 12:33:36 -0400

    fixed hit boxes

--- frogger/FroggerScreen.java
+++ frogger/FroggerScreen.java
@@ -56,40 +56,6 @@ public class FroggerScreen extends Screen {
         // Updates values regarding the frog's positioning.
         froggah.update();
         environmentManager.update();
-
-//        // Checks if the layer that the frog is on is a River so that it will check for collision on a log.
-//        if (environments.get(frogLevelPos) instanceof River) {
-//            ArrayList<Hazard> logs = environments.get(frogLevelPos).getHazards();
-//            for (int j = 0; j < logs.size(); j++) {
-//                logs.get(j).position.y = (600 - (screenBottomIndex * 30));
-//                if (!froggah.isOnLog() && logs.get(j).checkCollision(froggah)) {
-//                    froggah.setOnLog(true, logs.get(j).getSpeed());
-//                }
-//            }
-//
-//            // Checks if the frog is on a log so that it can change it's texture and cause a game over if necessary.
-//            if (!froggah.isOnLog()) {
-//                froggah.texture = new Texture("assets/frogger/water_line.png");
-//                //game over
-//            }
-//        }
-//
-//        // Checks if the layer that the frog is on is a Road so that it will check for collision with a car.
-//        if (environments.get(frogLevelPos) instanceof Road) {
-//            ArrayList<Hazard> cars = environments.get(frogLevelPos).getHazards();
-//            for (int j = 0; j < cars.size(); j++) {
-//                cars.get(j).position.y = (600 - (screenBottomIndex * 30));
-//                if (cars.get(j).checkCollision(froggah)) {
-//                    System.out.println("Frog dead");
-//                }
-//            }
-//
-//            // Checks if the frog is alive so that it can change it's texture and cause a game over if necessary.
-//            if (!froggah.isAlive()) {
-//                froggah.texture = new Texture("assets/frogger/grass_line.png");
-//                //game over
-//            }
-//        }
     }
 
     @Override
@@ -97,17 +63,17 @@ public class FroggerScreen extends Screen {
 
     }
 
-//    /**
-//     * This will check to see if the frog has made it to the end of the level.
-//     *
-//     * @return Whether or not the frog has made it to the end.
-//     */
-//    public boolean checkWin() {
-//        if (frogLevelPos == environments.size() - 15) {
-//            return true;
-//        }
-//        return false;
-//    }
+    /**
+     * This will check to see if the frog has made it to the end of the level.
+     *
+     * @return Whether or not the frog has made it to the end.
+     */
+    public boolean checkWin() {
+        if (environmentManager.posInLevel == environments.size() - 15) {
+            return true;
+        }
+        return false;
+    }
 
     /**
      * This output a game over screen if the frog dies.
